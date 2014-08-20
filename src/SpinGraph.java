855e87a47a5f5d559ff5bb97e14ccc80c4e5eec0
Daniel Bücheler <dbahn25@t-online.de>
2014-08-20 11:21:10 +0200

    energie berechnen

--- src/SpinGraph.java
+++ src/SpinGraph.java
@@ -1,4 +1,28 @@
-
 public class SpinGraph {
+	int[] ecken;
+	private float[][] kanten;
 
+	public SpinGraph(float[][] ecken) {
+		this.kanten = ecken;
+		this.ecken = new int[ecken.length];
+		System.out.println(energieBerechnen());
+	}
+
+	public float energieBerechnen() {
+		float energie = 0;
+		for(int i = 0; i < ecken.length; i++) {
+			ecken[i] = 1;
+		}
+		for (int i = 0; i < kanten.length; i++) {
+			for (int j = 0; j < kanten.length; j++) {
+//				if(kanten[i][j] == 0)
+//					System.out.println("null");
+//				else
+//					System.out.println(kanten[i][j]);
+				System.out.println(ecken[i] + " * " + ecken[j] +  " * " + kanten[i][j] + " = " + (kanten[i][j] * ecken[i] * ecken[j]));
+				energie += kanten[i][j] * ecken[i] * ecken[j];
+			}
+		}
+		return energie;
+	}
 }
--- src/SpinGraphApp.java
+++ src/SpinGraphApp.java
@@ -2,8 +2,12 @@
 public class SpinGraphApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		float[][] argument = new float[4][4];
+		argument[0][1] = -2;
+		argument[0][2] = 1.5f;
+		argument[1][2] = 3;
+		argument[2][3] = 5;
+		SpinGraph sg = new SpinGraph(argument);
 	}
 
 }
