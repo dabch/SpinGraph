767a94bef0b30550ae93d556d1575f8fafc4d7dc
Daniel Bücheler <dbahn25@t-online.de>
2014-08-20 19:08:25 +0200

    Refactoring 2. Einfacheres Erstellen von Kanten

--- src/SpinGraphApp.java
+++ src/SpinGraphApp.java
@@ -7,36 +7,38 @@ public class SpinGraphApp {
 
 	public static void main(String[] args) {
 		float[][] kanten = new float[9][9];
-		long startMillis;
+		long startMillis = System.currentTimeMillis();
 		long endMillis;
-		startMillis = System.currentTimeMillis();
-		kanten[0][1] = -5;
-		kanten[0][2] = 2;
-		kanten[0][6] = 4;
-		kanten[1][3] = -6;
-		kanten[1][6] = 3;
-		kanten[2][3] = -2;
-		kanten[2][4] = -4;
-		kanten[2][6] = -5;
-		kanten[2][7] = 16;
-		kanten[3][6] = 1;
-		kanten[3][5] = 1;
-		kanten[3][7] = -2;
-		kanten[4][7] = 2;
-		kanten[4][8] = 6;
-		kanten[5][8] = 4;
-		kanten[5][7] = 3;
-		kanten[7][8] = -4;
+		kanten = kantenEintragen();
 		SpinGraph sg = new SpinGraph(kanten);
 		sg.printPerfectConfig();
 		endMillis = System.currentTimeMillis();
 		System.out.println("Zeit zur Berechnung: " + (endMillis - startMillis) + " ms");
 	}
 	
-	public void eintragen(int ecke1, int ecke2, float wert){
-		
-	}
-	
-	
-
+	public static float[][] kantenEintragen() {
+		int knotenzahl = 9;
+		float[][] einzutragen = {
+				{0, 1, -5},
+				{0, 2, 2},
+				{0, 6, 4},
+				{1, 3, -6},
+				{1, 6, 3},
+				{2, 3, -2},
+				{2, 4, -4},
+				{2, 6, -5},
+				{2, 7, 16},
+				{3, 6, 1},
+				{3, 5, 1},
+				{3, 7, -2},
+				{4, 7, 2},
+				{4, 8, 6},
+				{5, 8, 4},
+				{5, 7, 3},
+				{7, 8, -4}};
+		float[][] kanten = new float[knotenzahl][knotenzahl];
+		for(float[] kante : einzutragen)
+			kanten[(int) kante[0]][(int) kante[1]] = kante[2];
+		return kanten;
+	}	
 }
