302936284a9d7b8580ad9f6f10c1f9ec05878617
Daniel Bücheler <dbahn25@t-online.de>
2014-08-21 18:49:20 +0200

    Simulated Amnealing hinzugefügt, FUNKTIONIERT NOCH NICHT!

--- src/SpinGraph.java
+++ src/SpinGraph.java
@@ -1,18 +1,31 @@
 public class SpinGraph {
+	final float SCHWELLE = 0.2f;
 	byte[] finalConfig;
 	byte[][] eckenKombinationen;
 	private float[][] kanten;
 	boolean minIsSet = false;
 	float min = 0;
 
-	public SpinGraph(float[][] kanten) {
+	public SpinGraph(float[][] kanten, int anzahlEcken) {
 		this.kanten = kanten;
 		this.finalConfig = null;
-		this.eckenKombinationen = new byte[(int) Math.pow(2, kanten.length)][kanten.length];
-		moeglichkeiten();
+		this.eckenKombinationen = new byte[(int) Math.pow(2, anzahlEcken)][anzahlEcken];
+		simualtedAmnealing(anzahlEcken);	
+	}
+	
+	
+	public void bruteForce() {
+		for(int i = 0; i < eckenKombinationen.length; i++) {
+			String iBinary = Integer.toBinaryString(i);
+			for(int j = 0; j < eckenKombinationen[i].length; j++) {
+				if(j < iBinary.length()) {
+					eckenKombinationen[i][eckenKombinationen[i].length - j - 1] = (byte) Integer.parseInt(String.valueOf(iBinary.charAt(iBinary.length() - j - 1)));
+				} else
+					eckenKombinationen[i][eckenKombinationen[i].length - j - 1] = 0;
+			}
+		}
 		for(byte[] config : eckenKombinationen) {
 			float energieConfig = energieBerechnen(config);
-//			System.out.println(energieConfig);
 			if(minIsSet){
 				min = energieConfig;
 				minIsSet = true;
@@ -24,30 +37,49 @@ public class SpinGraph {
 	}
 	
 	
-	public void moeglichkeiten() {
-		for(int i = 0; i < eckenKombinationen.length; i++) {
-			String iBinary = Integer.toBinaryString(i);
-			for(int j = 0; j < eckenKombinationen[i].length; j++) {
-				if(j < iBinary.length()) {
-					eckenKombinationen[i][eckenKombinationen[i].length - j - 1] = (byte) Integer.parseInt(String.valueOf(iBinary.charAt(iBinary.length() - j - 1)));
-				} else
-					eckenKombinationen[i][eckenKombinationen[i].length - j - 1] = 0;
-			}
-		}
-	}
-	
-	
 	public float energieBerechnen(byte[] config) {
 		float energie = 0;
-		for(int i = 0; i < config.length; i++)
-			config[i] = (byte) (config[i] * 2 -1);
+		byte[] tempConfig = config.clone();
+		for(int i = 0; i < tempConfig.length; i++)
+			tempConfig[i] = (byte) (tempConfig[i] * 2 -1);
 		
 		for (int i = 0; i < kanten.length; i++) {
 			for (int j = 0; j < kanten.length; j++) {
-				energie += kanten[i][j] * config[i] * config[j];
+				energie += kanten[i][j] * tempConfig[i] * tempConfig[j];
 			}
 		}
 		return energie;
+	}
+	
+	public void simualtedAmnealing(int anzahlEcken) {
+		finalConfig = new byte[anzahlEcken]; //
+	System.out.println(finalConfig.length);
+		boolean letztesEins = false;
+		for(int i = 0; i < finalConfig.length; i++) {
+			if(!letztesEins) {
+				finalConfig[i] = (byte) 1;
+				letztesEins = true;
+				System.out.println(finalConfig[i]);
+			} else {
+				finalConfig[i] = (byte) -1;
+				letztesEins = false;
+				System.out.println(finalConfig[i]);
+			}
+		}
+		for(int t= 1 ; t <= 50; t++){	
+			for(int j = 0; j < finalConfig.length; j++) {
+				float e1 = energieBerechnen(finalConfig);
+				finalConfig[j] *= -1;
+				float e2 = energieBerechnen(finalConfig);
+				finalConfig[j] *= -1;
+				if(e2 > e1) {
+					finalConfig[j] *= -1;
+				} else if(Math.pow(Math.E, (-e1 - e2/t)) > SCHWELLE) {
+					finalConfig[j] *= -1;
+				}
+			}
+		}
+		
 	}
 	
 	public void printPerfectConfig() {
--- src/SpinGraphApp.java
+++ src/SpinGraphApp.java
@@ -1,34 +1,39 @@
 
 /**
  * QuickAndDirtyStyle(TM) by Michael Brenner
- * @author Monty King, Oliver Butowski und Daniel Bücheler
+ * @author Monty King, Oliver Butowski und Daniel Bï¿½cheler
  *
  */
 public class SpinGraphApp {
 
 	public static void main(String[] args) {
-		float[][] argument = new float[9][9];
+		float[][] argument = new float[4][4];
 		long startMillis;
 		long endMillis;
 		startMillis = System.currentTimeMillis();
-		argument[0][1] = -5;
-		argument[0][2] = 2;
-		argument[0][6] = 4;
-		argument[1][3] = -6;
-		argument[1][6] = 3;
-		argument[2][3] = -2;
-		argument[2][4] = -4;
-		argument[2][6] = -5;
-		argument[2][7] = 16;
-		argument[3][6] = 1;
-		argument[3][5] = 1;
-		argument[3][7] = -2;
-		argument[4][7] = 2;
-		argument[4][8] = 6;
-		argument[5][8] = 4;
-		argument[5][7] = 3;
-		argument[7][8] = -4;
-		SpinGraph sg = new SpinGraph(argument);
+		
+//		argument[0][1] = -5;
+//		argument[0][2] = 2;
+//		argument[0][6] = 4;
+//		argument[1][3] = -6;
+//		argument[1][6] = 3;
+//		argument[2][3] = -2;
+//		argument[2][4] = -4;
+//		argument[2][6] = -5;
+//		argument[2][7] = 16;
+//		argument[3][6] = 1;
+//		argument[3][5] = 1;
+//		argument[3][7] = -2;
+//		argument[4][7] = 2;
+//		argument[4][8] = 6;
+//		argument[5][8] = 4;
+//		argument[5][7] = 3;
+//		argument[7][8] = -4;
+		argument[0][1] = 2;
+		argument[1][2] = 3;
+		argument[2][3] = 4;
+		
+		SpinGraph sg = new SpinGraph(argument, 4);
 		sg.printPerfectConfig();
 		endMillis = System.currentTimeMillis();
 		System.out.println("Zeit zur Berechnung: " + (endMillis - startMillis) + " ms");
